b61b19babccf3ce8fc9c0a8acd886e4a3d9c1a44
soukaina alayoua <salayoua@gmail.com>
2025-03-16 17:50:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navbar, Container, Nav, Card, Button, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* Navbar */}
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        {/* Heading */}
+        <h1>Welcome to My React App</h1>
+
+        {/* Cards */}
+        <Row>
+          <Col sm={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 1</Card.Title>
+                <Card.Text>
+                  This is a card.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 2</Card.Title>
+                <Card.Text>
+                  This is another card.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Card 3</Card.Title>
+                <Card.Text>
+                  This is the third card.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 }
 
