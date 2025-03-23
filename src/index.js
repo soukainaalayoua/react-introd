dd138c380f000e944a3737bb303cb46210134902
soukaina alayoua <salayoua@gmail.com>
2025-03-23 02:37:19 +0000

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';  // Import from 'react-dom/client'
 import App from './App';  // Import the App component
 import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Create root
+root.render(  // Use root.render() instead of ReactDOM.render()
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
